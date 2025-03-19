9f87219f946415000225a6ccef6cc2ee27612671
ngocphuc.1910 <ngocphuc159tl@gmail.com>
2025-03-19 21:02:31 +0700

    trying to handle stupid problem from Cursor

---
+++ src/hooks/useTask 2.js
@@ -0,0 +1,12 @@
+import { useContext } from 'react';
+import { TaskContext } from '../contexts/TaskContext';
+
+export const useTask = () => {
+  const context = useContext(TaskContext);
+  
+  if (!context) {
+    throw new Error('useTask must be used within a TaskProvider');
+  }
+  
+  return context;
+}; 
